97bd7a2e317dd2e3b8bf5df9e257e3eea2042b5d
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-07-24 16:17:28 +0300

    Proje dosyası ekle.

---
+++ MyPortfolioUdemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPortfolioUdemy.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
